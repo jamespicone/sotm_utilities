de260e2060779e8c178a643f7459362e9a5aed08
James Picone <jamesmpicone+github@gmail.com>
2022-02-13 18:40:30 +1100

    Add SelectTargetsToDealDamageToTarget method

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -66,5 +66,95 @@ namespace Jp.SOTMUtilities
                 co.GetCardSource()
             );
         }
+
+        public static IEnumerator SelectTargetsToDealDamageToTarget(
+            this CardController c,
+            HeroTurnTakerController decisionMaker,
+            Func<Card, bool> damageDealerCriteria,
+            Func<Card, bool> targetCriteria,
+            int amount,
+            DamageType damageType
+        )
+        {
+            return c.SelectTargetsToDealDamageToTarget(
+                decisionMaker,
+                damageDealerCriteria,
+                damageDealer => c.GameController.SelectTargetsAndDealDamage(
+                    decisionMaker,
+                    new DamageSource(c.GameController, damageDealer),
+                    amount,
+                    damageType,
+                    1,
+                    optional: false,
+                    requiredTargets: 0,
+                    additionalCriteria: card => targetCriteria(card),
+                    cardSource: c.GetCardSource()
+                )
+            );
+        }
+
+        public static IEnumerator SelectTargetsToDealDamageToTarget(
+            this CardController c,
+            HeroTurnTakerController decisionMaker,
+            Func<Card, bool> damageDealerCriteria,
+            Func<Card, IEnumerator> damageFunc
+        )
+        {
+            var alreadySelected = new List<Card>();
+            bool autoDecided = false;
+            IEnumerator e;
+
+            while (true)
+            {
+                var selectable = c.GameController.FindCardsWhere(card => card.IsInPlay && damageDealerCriteria(card), true, c.GetCardSource()).Except(alreadySelected);
+                if (selectable.Count() <= 0) { break; }
+
+                Card selectedDamageDealer;
+
+                if (autoDecided)
+                {
+                    selectedDamageDealer = selectable.First();
+                }
+                else
+                {
+                    var storedResults = new List<SelectCardDecision>();
+                    e = c.GameController.SelectCardAndStoreResults(
+                        decisionMaker,
+                        SelectionType.CardToDealDamage,
+                        new LinqCardCriteria(card => selectable.Contains(card)),
+                        storedResults,
+                        optional: false,
+                        allowAutoDecide: true,
+                        cardSource: c.GetCardSource()
+                    );
+                    if (c.UseUnityCoroutines)
+                    {
+                        yield return c.GameController.StartCoroutine(e);
+                    }
+                    else
+                    {
+                        c.GameController.ExhaustCoroutine(e);
+                    }
+
+                    selectedDamageDealer = storedResults.FirstOrDefault()?.SelectedCard;
+                    if (selectedDamageDealer == null) { break; }
+
+                    autoDecided = storedResults.FirstOrDefault().AutoDecided;
+                }
+
+                alreadySelected.Add(selectedDamageDealer);
+
+                e = damageFunc(selectedDamageDealer);
+
+                if (c.UseUnityCoroutines)
+                {
+                    yield return c.GameController.StartCoroutine(e);
+                }
+                else
+                {
+                    c.GameController.ExhaustCoroutine(e);
+                }
+            }
+        }
     }
 }
