2e458db64f53ab33386cd5a588d303ecf1e94fe4
James Picone <jamesmpicone+github@gmail.com>
2023-08-11 10:43:55 +1000

    Tests for null checks

---
+++ JpSOTMUtilitiesTests/AlignmentTests/GeneralAlignmentTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using Handelabra.Sentinels.Engine.Controller;
+
+namespace Jp.SOTMUtilities.UnitTest
+{
+    [TestFixture()]
+    public class GeneralAlignmentTests : BaseTest
+    {
+        [Test()]
+        public void TestNulls()
+        {
+            SetupGameController("BaronBlade", "Legacy", "Jp.SOTMUtilities.TestMod.AlignmentTestEnvironment");
+
+            Card card = null;
+            TurnTaker turntaker = null;
+            CardController controller = null;
+
+            Card realCard = baron.CharacterCard;
+            TurnTaker realTurntaker = baron.TurnTaker;
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)card.Is().Environment(); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)turntaker.Is().Environment(); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)controller.Is().Environment(); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)realCard.Is(controller).Environment(); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)realCard.Is().Environment().AccordingTo(controller); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)realTurntaker.Is(controller).Environment(); }
+            );
+
+            Assert.Throws<NullReferenceException>(
+                () => { bool b = (bool)realTurntaker.Is().Environment().AccordingTo(controller); }
+            );
+        }
+    }
+}
